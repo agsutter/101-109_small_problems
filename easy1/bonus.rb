b8bba4f25886e30ec93b8a0c08e9fc7ffe3a4e03
Amanda <amandagsutter@gmail.com>
2018-07-17 13:22:58 -0700

    change t_or_f to boolean

--- easy1/bonus.rb
+++ easy1/bonus.rb
@@ -3,7 +3,7 @@
 # true, the bonus should be half of the salary. If the boolean is false,
 # the bonus should be 0.
 
-def calculate_bonus(salary, t_or_f)
-  return 0 unless t_or_f
-  return salary.to_i / 2 if t_or_f
+def calculate_bonus(salary, boolean)
+  return salary.to_i / 2 if boolean
+  return 0
 end
